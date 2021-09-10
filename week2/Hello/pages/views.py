901fd2e278a1fbc47a18573097bf81a863ca156a
revedave <56846249+revedave@users.noreply.github.com>
2021-09-09 22:22:15 -0600

    Test

---
+++ week2/Hello/pages/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+from django.views.generic import TemplateView
+
+class AboutView(TemplateView):
+    template_name = 'page.html'
+    
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'About this Class', 
+            'body': 'Once upon a time ...',
+        }
+
+class HomeView(TemplateView):
+    template_name = "page.html"
+    
+    def get_context_data(self, **kwargs):
+        return {
+            'title': 'My Home Page', 
+            'body': 'This page is boring ...',
+        }
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+
