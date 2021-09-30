63bd702b3cbe6f7a3211814cf916aac32dd60b4a
revedave <56846249+revedave@users.noreply.github.com>
2021-09-29 21:39:57 -0600

    css and links

--- week5/Superhero/hero/models.py
+++ week5/Superhero/hero/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Superhero(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.CharField(max_length=200)
+    name = models.CharField(max_length=20)
+    identity = models.CharField(max_length=200)
 
-
+def __str__(self):
+        return f'{self.name} by {self.identity}'
