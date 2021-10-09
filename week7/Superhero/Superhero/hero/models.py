6bf9bf1055e311224000f144e4d1835d091fc1b4
revedave <56846249+revedave@users.noreply.github.com>
2021-10-08 18:39:31 -0600

    project 7

---
+++ week7/Superhero/Superhero/hero/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.urls.base import reverse_lazy
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=20)
+    identity = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    hero = models.CharField(max_length=200, default="00000", editable=True)  
+
+    def __str__(self):
+        return f'{self.name} {self.identity} {self.description}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('book_detail', args=[str(self.id)])
---
+++ week7/Superhero/Superhero/hero/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, RedirectView, UpdateView
+
+from .models import Superhero
+
+class HeroView(RedirectView):
+    url = '/hero/'
+
+class HeroListView(ListView):
+    template_name = 'hero_list.html'
+    model = Superhero
+
+
+class HeroDetailView(DetailView):
+    template_name = 'hero_detail.html'
+    model = Superhero
+
+class HeroCreateView(CreateView):
+    template_name = "hero_add.html"
+    model = Superhero
+    fields = ['name', 'identity', 'description']
+
+class HeroUpdateView(UpdateView):
+    template_name = "hero_edit.html"
+    model = Superhero
+    fields = ['name', 'identity', 'description']
+
+
+class HeroDeleteView(DeleteView):
+    tempplate_name = 'hero_delete.html'
+    model = Superhero
+    fields = ['name', 'identity', 'description']
