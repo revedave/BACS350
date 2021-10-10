dc6fc58b8291da096071045467c5fe9694851fe8
revedave <56846249+revedave@users.noreply.github.com>
2021-10-09 19:53:40 -0600

    week 6 progress

--- week6/Superhero/hero/models.py
+++ week6/Superhero/hero/models.py
@@ -5,12 +5,12 @@ class Superhero(models.Model):
     name = models.CharField(max_length=20)
     identity = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    strength = models.CharField(max_length= 200)
-    weakness = models.CharField(max_length=200)
-    hero = models.CharField(max_length=200, default="00000", editable=True)  
+    strength = models.CharField(max_length= 200, editable= True)
+    weakness = models.CharField(max_length=200, editable= True)
+    hero = models.CharField(max_length=200, editable=True)  
 
     def __str__(self):
-        return f'{self.name} {self.identity} {self.description}'
+        return f'{self.name} {self.identity} {self.description} {self.strength} {self.weakness}'
 
     def get_absolute_url(self):
-        return reverse_lazy('book_detail', args=[str(self.id)])
+        return reverse_lazy('hero_detail', args=[str(self.id)])
