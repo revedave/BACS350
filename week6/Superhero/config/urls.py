df89b7484db6fb2f7473ddb75b7594525e6707cb
revedave <56846249+revedave@users.noreply.github.com>
2021-10-10 20:05:02 -0600

    images do not work

--- week6/Superhero/config/urls.py
+++ week6/Superhero/config/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('hero/', HeroListView.as_view(),    name='hero_list'),
     path('hero/<int:pk>', HeroDetailView.as_view(),  name='hero_detail'),
     path('hero/add', HeroCreateView.as_view(),  name='hero_add'),
-    path('hero/<int:pk>/', HeroUpdateView.as_view(),  name='hero_edit'),
+    path('hero/<int:pk>/edit', HeroUpdateView.as_view(),  name='hero_edit'),
     path('hero/<int:pk>/delete', HeroDeleteView.as_view(),  name='hero_delete'),
 ]
