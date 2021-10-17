5ae5482754c07c4252b85a34051dafc222a24fdb
revedave <56846249+revedave@users.noreply.github.com>
2021-10-16 19:29:16 -0600

    week 8 start

---
+++ week8/Superhero/hero/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.urls.base import reverse_lazy
+
+class Superhero(models.Model):
+    name = models.CharField(max_length=20)
+    identity = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    strength = models.CharField(max_length= 200, editable= True)
+    weakness = models.CharField(max_length=200, editable= True)
+    hero = models.CharField(max_length=200, editable=True)  
+
+    def __str__(self):
+        return f'{self.name} {self.identity} {self.description} {self.strength} {self.weakness}'
+
+    def get_absolute_url(self):
+        return reverse_lazy('hero_detail', args=[str(self.id)])
